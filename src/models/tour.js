54afd138761883d50cee6fdc16a93e2e912d8753
Sumit Mondal <sumit.mondal.flailabs@extends.idfy.com>
2024-02-10 22:11:59 +0530

    fixed tour/matches

--- src/models/tour.js
+++ src/models/tour.js
@@ -6,10 +6,22 @@ const getAllTours = async () => {
     return await mysql.query(statement, parameters);
 }
 
-const getMatchesByTourName = async params => {
-    const statement = 'select * from matches left join tours on matches.tourId = tours.id where tours.name = ?';
-    const parameters = [ params.name ];
+const getTourDetails = async params => {
+    console.log("1111");
+    const statement = 'select * from tours where tours.name = ?';
+    const parameters = [params.name];
     return await mysql.query(statement, parameters);
+}
+
+const getMatchesByTourName = async params => {
+
+    return getTourDetails(params).then(async res => {
+        const [tour] = res
+        const statement = 'select * from matches where matches.tourId = ?';
+        const parameters = [tour.id];
+        return await mysql.query(statement, parameters);
+    })
+
 }
 
 module.exports = {
